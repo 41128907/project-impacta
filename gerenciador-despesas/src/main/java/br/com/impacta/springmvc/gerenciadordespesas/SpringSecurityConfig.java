588f06f9e42d224b8ca055c73ba68c0fc48bd308
di4515@classroom.net <di4515@classroom.net>
2018-03-08 07:41:02 -0300

    first commit

---
+++ gerenciador-despesas/src/main/java/br/com/impacta/springmvc/gerenciadordespesas/SpringSecurityConfig.java
@@ -0,0 +1,33 @@
+package br.com.impacta.springmvc.gerenciadordespesas;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin(); 			
+	}
+	
+	@Override
+	  public void configure(AuthenticationManagerBuilder builder) throws Exception {
+	    builder.inMemoryAuthentication()
+	           .withUser("joao").password("joao123")
+	           .roles("USUARIO_COMUM")
+	           .and()
+	           .withUser("maria").password("maria123")
+	           .roles("USUARIO_COMUM");
+	}
+	
+	
+	
+}
