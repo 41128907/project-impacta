513567436739bcf58ebf35bf61be0f79dc06bb1a
di4515@CLASSROOM.NET <di4515@CLASSROOM.NET>
2018-03-08 07:46:27 -0300

    change project

--- gerenciador-despesas/src/main/java/br/com/impacta/springmvc/gerenciadordespesas/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package br.com.impacta.springmvc.gerenciadordespesas;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
